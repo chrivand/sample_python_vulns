addc7aaf41592914c64d36f4ea9b02150a7af3a7
chrivand <chrivand@cisco.com>
2022-03-18 11:59:14 +0100

    Update py_vuln03.py

--- py_vuln03.py
+++ py_vuln03.py
@@ -8,3 +8,5 @@ assert x == "goodbye"
 
 # if condition returns False, custom AssertionError is raised: 
 assert x == "goodbye", "x should be 'hello'"
+
+# run like this to disable asserts: python -O py_vuln03.py
