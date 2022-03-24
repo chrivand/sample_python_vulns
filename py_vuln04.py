a76d5645d28ff403dadb5836514c5307e4367418
chrivand <chrivand@cisco.com>
2022-03-24 16:37:23 +0100

    Create py_vuln04.py

---
+++ py_vuln04.py
@@ -0,0 +1,9 @@
+def update_details(request, acc_id):  
+  user = Account.objects.get(acc=acc_id)  
+  if request.user.id == user.id:  
+    # ALLOW ACTION  
+    # VALIDATE REQUEST DATA  
+    form = AccountForm(instance=user,request=request)  
+    ...  
+  else:  
+ 	  # DENY ACTION 
