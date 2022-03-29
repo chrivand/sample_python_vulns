1407809c897c274f64b7066c8d39b97c20f00241
chrivand <chrivand@cisco.com>
2022-03-29 16:04:23 +0200

    Update py_vuln00.py

--- py_vuln00.py
+++ py_vuln00.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# example code snippet py_vuln00: Arbitrary Code Execution
+# example code snippet py_vuln00: Arbitrary Code Execution:
 compute_user_input = input('\nType something here to compute: ')
 if not compute_user_input:
 	print ("No input")
